20233cf8ed5c3bff2f1600958d46a4dcec7ca2d0
GITHUBPWM <793597319@qq.com>
2020-10-30 18:36:46 +0800

    引入公司组件库

--- G5328x/src/views/commonFunc/vlan/QVlan.vue
+++ G5328x/src/views/commonFunc/vlan/QVlan.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
+
     <div class="form-button-group">
       <button class="btn btn-title">添加</button>
       <button class="btn disabled">删除</button>
     </div>
-    <v-table ref="table"  ></v-table>
+    <v-table ref="table"  :tableOptions="tableData" @on-custom-comp="customCompFunc" :callback="afterUpdateTable"></v-table>
   </div>
 </template>
 
@@ -13,13 +14,49 @@
       name: "QVlan",
       data(){
         return {
-          tableData:[],
+          tableData:{
+            key: "qvlan",
+            css: "",
+            columns: [
+              {
+                title: "VLAN ID",
+                field: "vlanId"
+              },
+              {
+                title: "VLAN描述",
+                field: "remark",
+              },
+              {
+                title: "IPv4地址",
+                field: "ip",
+              },
+              {
+                title: "子网掩码",
+                field: "mask",
+              },
+              {
+                title: "操作",
+                // componentName: "OperateBtGroup",
+              }
+            ]
+          },
         }
+      },
+
+      methods:{
+        customCompFunc(){
+
+        },
+
+        afterUpdateTable(){
+          alert("更新表格");
+        }
+
       },
 
       mounted() {
         this.$axios.get("/goform/getQvlanList").then((res)=>{
-          this.tableData = res.data;
+          this.tableData.originData = res.data.list;
         })
       }
 
